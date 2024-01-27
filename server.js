978edb64e0cf608242405d5801c25f25042870ac
LarisaYakovenko <133209659+LarisaYakovenko@users.noreply.github.com>
2024-01-27 17:26:10 +0200

    add .env

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 import app from "./app"
 
-// import DB_HOST from "./config.js"
 const { DB_HOST } = process.env;
 
 mongoose.connect(DB_HOST)
   .then(() => {
-  console.log('Database connection successful');
+    app.listen(3001)
+  // console.log('Database connection successful');
 }).catch((error) => {
   console.log(error.message);
   process.exit(1);
